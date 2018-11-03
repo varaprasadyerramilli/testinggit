811eceaa48e5c8f03d4078ce7f155788261fd114
varaprasad <varaprasad@192.168.0.9>
2018-11-03 08:19:40 -0700

    Changed the code

--- Baseclass.java
+++ Baseclass.java
@@ -9,20 +9,9 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Baseclass {
 	
-	public static WebDriver driver;
-	public WebDriver browserinvoking() throws IOException
+	public static void main(String[] args)
 	{
-		Properties prop=new Properties();
-		FileInputStream fis=new FileInputStream("E:\\USEFULL SOFTWARES\\artifactidistheprojectname\\invoking.properties");
-		prop.load(fis);
-		String browsername=prop.getProperty("browser");
-		System.out.println(browsername);
-		if(browsername.equals("InternetExplorer"))
-		{
-			System.setProperty("webdriver.ie.driver","E:\\Selineum Learning Documentatin\\IEDriverServer_Win32_3.6.0\\IEDriverServer.exe");
-			driver=new InternetExplorerDriver();
-		}
-		return driver;
+		System.out.println("Hello World");
 	}
 	
 
